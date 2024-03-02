6af8f6bad18025cd81427732a109bd68e5a2d7bd
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2024-03-02 22:39:12 +0100

    add some sections to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,40 @@
 import Image from "next/image";
+import RightBar from "./Components/rightbar";
+import Banner from "./Components/homebanner";
+import FlashSales from "./Components/flashsales";
+import Categories from "./Components/categories";
+import ThisMonth from "./Components/thismonth";
+import MiddleBanner from "./Components/middlebanner";
+import OurProducts from "./Components/ourproducts";
+import Featured from "./Components/featured";
 
 export default function Home() {
   return (
-    <div className="bg-white h-screen">
+    <div className="w-full h-full">
+    <div className="bg-white h-screen flex flex-row ">
+      <RightBar /> 
+      <div className="absolute ml-[400px]">
+      <Banner />
+      </div>
+    </div>
+      <FlashSales />
+      <div className="h-[1px] w-[1300px] mx-auto bg-gray-400"></div>
+      <div >
+        <Categories />
+        
+      </div>
+      <div>
+        <ThisMonth />
+      </div>
+      <div>
+        <MiddleBanner />
+      </div>
+      <div>
+        <OurProducts />
+      </div>
+      <div>
+        <Featured />
+      </div>
     </div>
   );
 }
