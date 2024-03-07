d9db2d6baf31036b67be355c150bfaddd8fdd5b2
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2024-03-07 08:59:33 +0100

    done with home page responsivness

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,21 @@ import MiddleBanner from "./Components/middlebanner";
 import OurProducts from "./Components/ourproducts";
 import Featured from "./Components/featured";
 
+
 export default function Home() {
   return (
     <div className="w-full h-full">
     <div className="bg-white h-screen flex flex-row ">
       <RightBar /> 
-      <div className="absolute ml-[400px]">
+      <div className="lg:ml-20 lg:mt-5 mx-auto">
       <Banner />
       </div>
     </div>
+   
+    <div> 
+      
       <FlashSales />
-      <div className="h-[1px] w-[1300px] mx-auto bg-gray-400"></div>
+    </div>
       <div >
         <Categories />
         
